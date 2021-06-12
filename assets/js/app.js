ac0bb646ca3cf9d1a19d64abd97c1884eb0a960b
Anderson Koffi N'da <koffiandersonnda@gmail.com>
2021-06-12 16:06:17 +0000

    First commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,19 +29,95 @@ const linkAction = () => {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsHeader = document.querySelectorAll('.skills__header')
 
+skillsHeader.forEach(element => {
+    
+    element.addEventListener('click', () => {
+        const elementParent = element.parentNode; // Select element node parent
+
+        // if skills__open class exist in classList of element parent node remove it else add it
+        elementParent.classList.toggle('skills__open');  
+        
+    });
+    
+});
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+
+        target.classList.add('qualification__active')
+
+        tab.classList.toggle('qualification__active')
+
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
 
+const modal = (modalClick) => {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+        console.log('c fait')
+    })
+})
+
+modalCloses.forEach(modalClose => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach(modalView => {
+            modalView.classList.remove('active-modal')
+        });
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
 
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+        568:{
+            slidesPerView: 2,
+        }
+    }
+  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
